5981b823660081bcacca9a088dfba4e4ac53a40e
tik <thiagoiklock@gmail.com>
2023-12-28 11:09:10 -0300

    second blog

--- mysite/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
